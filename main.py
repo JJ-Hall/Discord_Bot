e3a711d58b523d1c1ce1e49dc55d04bbe91dddf2
Josh <joshhall1991@gmail.com>
2022-06-27 22:16:42 -0500

    added cleranced preworkout updater

--- main.py
+++ main.py
@@ -8,6 +8,8 @@ import time
 import asyncio
 import os
 from dotenv import load_dotenv
+from bs4 import BeautifulSoup
+
 
 load_dotenv()
 CHANNELID = int(os.getenv('CHANNELID'))
@@ -20,15 +22,51 @@ bot = commands.Bot(command_prefix='.')
 @bot.event
 async def on_ready():
     print('Bot is ready')
-    await holywater()
+    await supplements()
+    await grabFreeGames()
 
 @bot.command()
 async def ping(ctx):
     await ctx.send('pooong')
-    
 
 @tasks.loop()
-async def holywater():
+async def supplements():
+    currentData = []
+
+    while True:
+        channel = bot.get_channel(CHANNELID)
+        currentTime = time.time()
+        thenTime = currentTime + 1800
+        waitTime = thenTime - currentTime
+        await asyncio.sleep(waitTime)
+
+        url = 'https://supplementhunt.com/pages/last-chance'
+        response = requests.get(url)
+        soup = BeautifulSoup(response.content, 'html5lib')
+        section = soup.find('div', attrs = {'id':'63adad6a-2628-45b7-85b8-50da266a21ad'}) 
+        sectionItems = section.findAll('li')
+        itemData = []
+
+        for item in sectionItems:
+            itemTitle = item.h2.get_text()
+            itemOPriceDiv = item.find(class_= 'price__compare-at')
+            itemOPrice= itemOPriceDiv.find(class_ = 'money').get_text()
+            itemCPriceDiv = item.find(class_= 'price__current')
+            itemCPrice= itemCPriceDiv.find(class_ = 'money').get_text()
+            itemUrl = item.a['href']
+            actualUrl = f'https://supplementhunt.com/{itemUrl}'
+
+            itemData.append(itemUrl)
+
+            if itemUrl not in currentData:
+                await channel.send(f'{itemTitle}\nOrginal Price: {itemOPrice}\nSale Price: {itemCPrice}\n{actualUrl}')
+
+        currentData = itemData
+        itemData= []
+
+
+@tasks.loop()
+async def grabFreeGames():
     while True:
         channel = bot.get_channel(CHANNELID)
         currentTime = time.time()
